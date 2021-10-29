75621f3e2992bee2e4eea7a3aeb2438aeaaa7ae4
Marlon-Yuri <marlonyurimi@gmail.com>
2021-10-29 12:19:33 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "My Gatsby Site",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+};
