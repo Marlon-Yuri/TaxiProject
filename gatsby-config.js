f19e9a3b9cc7fbfee6394d6cd023eb9f3e2fa06c
Fernando <fernandocesarrussiefelip@gmail.com>
2021-11-02 10:00:32 -0300

    update footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,14 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Gatsby Site",
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-styled-components",
+  {
+    resolve: "gatsby-source-graphql",
+    options: {
+      typeName: "alldata",
+      fieldName: "alldata",
+      url: "https://api-us-east-1.graphcms.com/v2/ckv72oolc50e501xl8lkggor3/master",
+    },
+  },
+],
 };
